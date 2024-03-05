a4adf0e5116e2dfda426d22ea90bbf674f6c43ac
anmun <anmun2004@gmail.com>
2024-03-05 21:07:20 +0700

    hhaha

---
+++ app/src/main/java/com/example/chitiet/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.chitiet;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar =  getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+}
